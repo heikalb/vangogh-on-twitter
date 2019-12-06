35e33c7d4230b436628e0d1718b9821ece2ae447
heikalb <heikal93@gmail.com>
2019-12-05 22:33:54 -0800

    finished. Need to fully run to get data

--- get_data.py
+++ get_data.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def get_urls(url_template, first, last):
     urls = []
 
@@ -17,10 +18,10 @@ def get_letter(url):
     html = req.text
     soup = BeautifulSoup(html, 'html.parser')
 
-    letter_container = soup.select('#tab-container-1')
-    print(letter_container)
-
-
+    letter_container = soup.select('#tab-container-1')[0]
+    letter_texts = [div.text for div in letter_container.select('div')]
+    letter_text = ' '.join(letter_texts)
+    return letter_text
 
 
 def get_letters(urls):
@@ -32,13 +33,21 @@ def get_letters(urls):
     return letters
 
 
+def save_data(letters):
+    with open('vangogh_letters.txt', 'w') as f:
+        f.write('\n'.join(letters))
+
+
 def main():
     # Get URLs to get letter texts from
     url_template = 'http://vangoghletters.org/vg/letters/let{}/letter.html'
     urls = get_urls(url_template, 1, 902)
 
+    # Get letter texts
     letters = get_letters(urls)
-    return
+
+    # Save data
+    save_data(letters)
 
 
 if __name__ == '__main__':
